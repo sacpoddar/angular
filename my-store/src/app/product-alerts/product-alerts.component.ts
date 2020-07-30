9eb9d7f8b7b61ae0f422554148a80955c3e4c4ce
sachin <sac.poddar@gmail.com>
2020-07-30 16:16:19 +0530

    Fixed #1 Implement my-store app

---
+++ my-store/src/app/product-alerts/product-alerts.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { Input } from '@angular/core';
+
+@Component({
+  selector: 'app-product-alerts',
+  templateUrl: './product-alerts.component.html',
+  styleUrls: ['./product-alerts.component.css']
+})
+export class ProductAlertsComponent implements OnInit {
+  // @Input - indicates that the property value passes in from the component's parent
+  @Input() product; 
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
